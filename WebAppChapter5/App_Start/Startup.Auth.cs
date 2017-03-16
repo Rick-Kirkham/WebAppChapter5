a4426d188a2ce671cd50175aa6d82a544d972534
Rick Kirkham <rickeeee@msn.com>
2017-03-15 22:01:08 -0700

    initial commit

---
+++ WebAppChapter5/App_Start/Startup.Auth.cs
@@ -0,0 +1,24 @@
+﻿using Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+
+namespace WebAppChapter5
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    ClientId = "b8cd2a65-ab87-430b-9271-778778e1c50f",
+                    Authority = "https://login.microsoftonline.com/microsoft492.onmicrosoft.com",
+                    PostLogoutRedirectUri = "https://localhost:44320"
+                }
+            );
+        }
+    }
+}
---
+++ WebAppChapter5/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppChapter5.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        public void SignIn()
+        {
+            // Send OpenId Connect sign-in request.
+            if (!Request.IsAuthenticated)
+            {
+                HttpContext.GetOwinContext().Authentication.Challenge(
+                    new Microsoft.Owin.Security.AuthenticationProperties
+                    {
+                        RedirectUri = "/"
+                    },
+                    OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            }
+        }
+
+        public void SignOut()
+        {
+            HttpContext.GetOwinContext().Authentication.SignOut(
+                OpenIdConnectAuthenticationDefaults.AuthenticationType,
+                CookieAuthenticationDefaults.AuthenticationType);
+        }
+    }
+}
