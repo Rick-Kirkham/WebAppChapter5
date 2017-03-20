78a0cd4e59f4c04d99c49935fe9367759bc05d5b
Rick Kirkham <rickeeee@msn.com>
2017-03-20 12:28:17 -0700

    some changes from chapter 9

--- WebAppChapter5/App_Start/Startup.Auth.cs
+++ WebAppChapter5/App_Start/Startup.Auth.cs
@@ -2,6 +2,13 @@
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OpenIdConnect;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System.Web;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace WebAppChapter5
 {
@@ -16,7 +23,41 @@ namespace WebAppChapter5
                 {
                     ClientId = "b8cd2a65-ab87-430b-9271-778778e1c50f",
                     Authority = "https://login.microsoftonline.com/microsoft492.onmicrosoft.com",
-                    PostLogoutRedirectUri = "https://localhost:44320"
+                    PostLogoutRedirectUri = "https://localhost:44320",
+                    Notifications = new OpenIdConnectAuthenticationNotifications()
+                    {
+                        AuthorizationCodeReceived = (context) =>
+                        {
+                            Debug.WriteLine("*** Auth code received");
+                            string ClientId = "b8cd2a65-ab87-430b-9271-778778e1c50f";
+                            string Authority = "https://login.microsoftonline.com/microsoft492.onmicrosoft.com";
+                            string appKey = "OW3BzKvXdry/A6FOrtrPltjW2ZRebnniaVXIppRKWZw=";
+                            string resourceId = "https://graph.windows.net";
+                            var code = context.Code;
+                            AuthenticationContext authContext = new AuthenticationContext(Authority);
+                            ClientCredential credential = new ClientCredential(ClientId, appKey);
+                            AuthenticationResult result = authContext.AcquireTokenByAuthorizationCode(code,
+                                new System.Uri(HttpContext.Current.Request.Url.GetLeftPart(UriPartial.Path)),
+                                credential,
+                                resourceId);
+
+                            //string callOutcome = string.Empty;
+                            //HttpClient httpClient = new HttpClient();
+                            //httpClient.DefaultRequestHeaders.Authorization = 
+                            //    new AuthenticationHeaderValue("Bearer", result.AccessToken);
+                            //HttpResponseMessage response = 
+                            //    httpClient.GetAsync("https://graph.windows.net/me?api-version=1.6").Result;
+                            //if (response.IsSuccessStatusCode)
+                            //{
+                            //    callOutcome = response.Content.ReadAsStringAsync().Result;
+                            //}
+
+
+
+
+                            return Task.FromResult(0);
+                        }
+                    }
                 }
             );
         }
--- WebAppChapter5/Controllers/HomeController.cs
+++ WebAppChapter5/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.Owin.Security.Cookies;
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OpenIdConnect;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
@@ -18,8 +21,28 @@ namespace WebAppChapter5.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            string ClientId = "b8cd2a65-ab87-430b-9271-778778e1c50f";
+            string Authority = "https://login.microsoftonline.com/microsoft492.onmicrosoft.com";
+            string appKey = "OW3BzKvXdry/A6FOrtrPltjW2ZRebnniaVXIppRKWZw=";
+            string resourceId = "https://graph.windows.net";
 
+            
+            ClientCredential credential = new ClientCredential(ClientId, appKey);
+            AuthenticationContext authContext = new AuthenticationContext(Authority);
+
+            AuthenticationResult result = authContext.AcquireTokenSilent(resourceId,credential,UserIdentifier.AnyUser);
+
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", result.AccessToken);
+            HttpResponseMessage response =
+                httpClient.GetAsync("https://graph.windows.net/me?api-version=1.6").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                ViewBag.Message = response.Content.ReadAsStringAsync().Result;
+            }
+
+            
             return View();
         }
 
